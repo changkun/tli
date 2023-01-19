fe95c05096ef268971c80e88073cc9e6137076da
qcrao <qcrao91@gmail.com>
2023-01-19 20:08:04 +0800

    fix: update version to v0.2.1 (#5)
    * fix: update version to v0.2.1
    
    Co-authored-by: bob.rao <bob.rao@bybit.com>
    Co-authored-by: Changkun Ou <hi@changkun.de>

--- tli.go
+++ tli.go
@@ -28,7 +28,7 @@ import (
 
 // build info
 var (
-	Version     = "v0.1.0"
+	Version     = "v0.2.1"
 	GoVersion   = runtime.Version()
 	errCanceled = errors.New("action canceled")
 	homedir     string
